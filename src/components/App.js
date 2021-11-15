727be49c288d290cadb9b8b812c36f8f99a00d59
hu100 <84121935+epath2c@users.noreply.github.com>
2021-11-15 14:59:18 +0800

    modified the original input value

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import useVideos from "../hooks/useVideos";
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const [videos, search] = useVideos("harry potter");
+  const [videos, search] = useVideos("landscape");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
